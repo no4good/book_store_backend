a04fe2aa6f74e27fb3bed61f4b1742c1c065a871
no4good <no4goodll@gmail.com>
2019-10-12 23:11:52 +0300

    first steps

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send('hellss');
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(` Server runing ${port}`);
+
+})
+
