4f5612ec1cba1d41ac54acefad58e95ceed7c924
no4good <no4goodll@gmail.com>
2019-10-18 20:33:06 +0300

    add CRUD poduct, categories and list categories

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read, remove, update, list } = require('../controllers/category');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/categories', list)
 
+router.param('categoryId', categoryById)
 router.param('userId', userById);
 
 module.exports = router;
